c874ad80723e3076a72b486f92416ea4ffea1d76
Kuplumosk <kuplumoskk@gmail.com>
2021-02-10 11:42:04 +0700

    Init

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,6 +1,10 @@
 package koschei.config;
 
+import koschei.models.Deth8;
+import koschei.models.Duck5;
+import koschei.models.Egg6;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,4 +18,10 @@ public class AppConfig {
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+    @Bean
+    public static Duck5 getDuck(Egg6 egg) { return new Duck5(egg);}
+
+    @Bean
+    public static Needle7 getNeedle(Deth8 deth) {return new Needle7();}
 }
